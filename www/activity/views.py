0c0c9276bbc219cefe084315563c8ec0cbc91c5d
lei286394973 <286394973@qq.com>
2014-08-06 20:26:28 +0800

    活动页面

--- www/activity/views.py
+++ www/activity/views.py
@@ -9,3 +9,13 @@ from django.shortcuts import render_to_response
 def activity_list(request, template_name='activity/activity_list.html'):
 
     return render_to_response(template_name, locals(), context_instance=RequestContext(request))
+
+
+def activity_detail(request, activity_id=None, template_name='activity/activity_detail.html'):
+
+    return render_to_response(template_name, locals(), context_instance=RequestContext(request))
+
+
+def launch_activity(request, template_name='activity/launch_activity.html'):
+
+    return render_to_response(template_name, locals(), context_instance=RequestContext(request))
